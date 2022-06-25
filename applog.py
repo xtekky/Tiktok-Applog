a0ce4162ea09772c95920b8b05ca2c0d47fdb5cf
Tekky <98614666+xtekky@users.noreply.github.com>
2022-06-25 19:16:20 +0200

    Create applog.py

---
+++ applog.py
@@ -0,0 +1,41 @@
+# Contact me to purchase the code: t.me/xtekky
+# This is just a preview
+
+import random, time, json, requests, uuid; from collections import OrderedDict
+
+
+class Applog():
+    def __init__(self) -> None:
+        pass
+
+    def xxxxxxxxxxxxxxxxxxxxxxxxxxxx(self):
+        self.xxxxxx       = 'xxxxx'
+        self.xxxxxxxxxxx   = 'xxxxx'
+        self.xxxxxxxxxxxx   = 'xxxxx'
+        self.xxxxxxxxx   = 'xxxxx'
+        self.xxxxxx       = 'xxxxx'
+        self.xxxxxx       = 'xxxxx'
+        self.xxxxxx       = 'xxxxx'
+        self.xxxxxx       = 'xxxxx'
+        self.xxxxxxl      = 'xxxxx'
+
+        Data = xxxxxxx
+        return Data
+
+    def xxxxxxxxxxxxxxxxxxx(self):
+        payload = self.get_payload()
+        
+        req = requests.post(
+            url = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            headers = {
+                'user-agent': 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
+            },
+            data = payload
+        )
+        return json.dumps(req.json(), indent=4) #response with registered device id
+
+if __name__ == "__main__":
+    applog = Applog()
+    device_id = applog.get_device()
+
+    print(device_id)
